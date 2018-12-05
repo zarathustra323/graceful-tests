3cf8065546ff7ed36e1737b5a054d9505b29727f
Jacob Bare <jacob@limit0.io>
2018-12-05 09:51:25 -0600

    Enable ignoreUndefined

--- src/mongodb.js
+++ src/mongodb.js
@@ -7,6 +7,7 @@ module.exports = new MongoClient(MONGO_URI, {
   appname: `${name} v${version}`,
   connectTimeoutMS: 200, // TCP Connection timeout setting (default 30000)
   useNewUrlParser: true,
+  ignoreUndefined: true,
   // Single server options. Has no effect on RS members?
   reconnectTries: 15, // Default 30
   reconnectInterval: 500, // Default 1000
