065a47d31e8f2dd9e024098825566916fa411741
Jacob Bare <jacob@limit0.io>
2018-12-05 09:31:25 -0600

    Add version to mongodb app name

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
-const pkg = require('../package.json');
+const { name, version } = require('../package.json');
 
 const { MONGO_URI } = process.env;
 
 module.exports = new MongoClient(MONGO_URI, {
-  appname: pkg.name,
+  appname: `${name} v${version}`,
   connectTimeoutMS: 200, // TCP Connection timeout setting (default 30000)
   useNewUrlParser: true,
   // Single server options. Has no effect on RS members?
